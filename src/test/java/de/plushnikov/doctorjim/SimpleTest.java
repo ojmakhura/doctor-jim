d041686b979af9f92f3e16cde8e1d5f9bee71af7
plushnikov@web.de <plushnikov@web.de@aa42e070-007c-11df-b4af-25c34a55dae2>
2010-01-22 16:40:40 +0000

    added org.junit.Test import

--- src/test/java/de/plushnikov/doctorjim/SimpleTest.java
+++ src/test/java/de/plushnikov/doctorjim/SimpleTest.java
@@ -3,6 +3,7 @@ package de.plushnikov.doctorjim;
 import de.plushnikov.doctorjim.javaparser.ParseException;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -21,204 +22,209 @@ public class SimpleTest {
         mProcessor = new ImportProcessor();
     }
 
-    @org.junit.Test
+    @Test
     public void testOne() throws IOException, ParseException {
         testBeautifikation("Temp");
     }
 
-    @org.junit.Test
+    @Test
     public void testTwo() throws IOException, ParseException {
         testBeautifikation("Temp2");
     }
 
-    @org.junit.Test
+    @Test
     public void testThree() throws IOException, ParseException {
         testBeautifikation("package-info");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample1() throws Exception {
         testBeautifikation("Sample01");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample2() throws Exception {
         testBeautifikation("Sample02");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample3() throws Exception {
         testBeautifikation("Sample03");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample4() throws Exception {
         testBeautifikation("Sample04");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample5() throws Exception {
         testBeautifikation("Sample05");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample6() throws Exception {
         testBeautifikation("Sample06");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample7() throws Exception {
         testBeautifikation("Sample07");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample8() throws Exception {
         testBeautifikation("Sample08");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample9() throws Exception {
         testBeautifikation("Sample09");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample10() throws Exception {
         testBeautifikation("Sample10");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample11() throws Exception {
         testBeautifikation("Sample11");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample12() throws Exception {
         testBeautifikation("Sample12");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample13() throws Exception {
         testBeautifikation("Sample13");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample14() throws Exception {
         testBeautifikation("Sample14");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample15() throws Exception {
         testBeautifikation("Sample15");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample16() throws Exception {
         testBeautifikation("Sample16");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample17() throws Exception {
         mProcessor.setStrict(false);
         testBeautifikation("Sample17");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample18() throws Exception {
         mProcessor.setStrict(false);
         testBeautifikation("Sample18");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample20() throws Exception {
         testBeautifikation("Sample20");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample21() throws Exception {
         testBeautifikation("Sample21");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample22() throws Exception {
         testBeautifikation("Sample22");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample23() throws Exception {
         testBeautifikation("Sample23");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample24() throws Exception {
         testBeautifikation("Sample24");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample25() throws Exception {
         testBeautifikation("Sample25");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample26() throws Exception {
         testBeautifikation("Sample26");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample27() throws Exception {
         testBeautifikation("Sample27");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample28() throws Exception {
         testBeautifikation("Sample28");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample29() throws Exception {
         testBeautifikation("Sample29");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample30() throws Exception {
         testBeautifikation("Sample30");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample31() throws Exception {
         testBeautifikation("Sample31");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample32() throws Exception {
         testBeautifikation("Sample32");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample33() throws Exception {
         testBeautifikation("Sample33");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample34() throws Exception {
         testBeautifikation("Sample34");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample35() throws Exception {
         testBeautifikation("Sample35");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample36() throws Exception {
         testBeautifikation("Sample36");
     }
 
-    @org.junit.Test
+    @Test
     public void testSample37() throws Exception {
         testBeautifikation("Sample37");
     }
 
-
+  /**
+   * Execute beautifikation on the given file and compare result
+   * @param pFilename  filename to be used as input for beautifikation
+   * @throws IOException
+   * @throws ParseException
+   */
     private void testBeautifikation(String pFilename)
             throws IOException, ParseException {
         String lInput = IOUtils.toString(this.getClass().getResourceAsStream(pFilename + ".java_input"));
